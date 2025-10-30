44c8dac094d003c855e58e786195ef51eb7a11e4
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-10-30 13:51:01 -0700

    first commit

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My website</title>
+</head>
+<body>
+    <nav>
+        <x-nav-link href="/">Home</x-nav-link>
+        <x-nav-link href="/about">about</x-nav-link>
+        <x-nav-link href="/contact">contact</x-nav-link>
+    </nav>
+    <div>
+{{ $slot }}
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>example</title>
+</head>
+<body>
+    <x-layout>
+        <h1>this is home page</h1>
+    </x-layout>
+</body>
+</html>
