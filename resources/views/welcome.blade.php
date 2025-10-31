6163cc8b58819a48f507c1230957c2e41bfe531e
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-10-31 10:40:35 -0700

    Refactor layout and add heading slots to pages
    Replaced custom nav-link component with a Tailwind-based navigation bar in layout.blade.php and removed nav-link.blade.php. Added heading slots to about, contact, and welcome pages for improved structure and consistency.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>example</title>
-</head>
-<body>
+
     <x-layout>
+        <x-slot:heading>
+Home Page
+        </x-slot:heading>
         <h1>this is home page</h1>
+
     </x-layout>
-</body>
-</html>
