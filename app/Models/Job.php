444c6715c5ceeddf5b71492659a9ddfa30d148c2
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-11-04 21:48:14 +0300

    Handle missing job with 404 in Job::find
    Updated Job::find to abort with a 404 error if the job is not found, improving error handling for invalid job IDs. Also removed unused Arr import from web.php.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -32,6 +32,11 @@ return  $jobs = [
 
 public static function find(int $id): array
 {
-   return Arr::first(static::all(), fn($job) => $job['id'] == $id);
+   $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+   if (!$job) {
+       abort(404);
+   }
+    return $job;
 }
 }
